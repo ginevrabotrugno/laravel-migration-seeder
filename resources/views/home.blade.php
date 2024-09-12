6162403ccf8c566da13393b974c442bd099f6fc6
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-09-12 18:18:29 +0200

    feat: print trains in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,46 @@
 
 @section('content')
 
-@dump($trains)
+<div class="container my-5">
+
+    <h1>TRAINS</h1>
+
+    <table class="table text-center">
+        <thead>
+          <tr>
+            <th scope="col">Train Code</th>
+            <th scope="col">Company</th>
+            <th scope="col">Departure Time</th>
+            <th scope="col">Arrival Time</th>
+            <th scope="col">On Time</th>
+            <th scope="col">Departure Station</th>
+            <th scope="col">Arrival Station</th>
+            <th scope="col">Carriage Number</th>
+            <th scope="col">Cancelled</th>
+          </tr>
+        </thead>
+        <tbody class="table-group-divider">
+            @foreach ($trains as $train)
+
+                <tr>
+                    <th>  {{ $train->train_code }} </th>
+                    <td>  {{ $train->company }} </td>
+                    <td>  {{ $train->departure_time }} </td>
+                    <td>  {{ $train->arrival_time }} </td>
+                    <td>  {{ $train->on_time }} </td>
+                    <td>  {{ $train->departure_station }} </td>
+                    <td>  {{ $train->arrival_station }} </td>
+                    <td>  {{ $train->carriage_number }} </td>
+                    <td>  {{ $train->cancelled }} </td>
+                </tr>
+
+            @endforeach
+        </tbody>
+      </table>
+
+</div>
+
+
 
 @endsection
 
