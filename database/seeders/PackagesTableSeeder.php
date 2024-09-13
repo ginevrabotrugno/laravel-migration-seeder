e9868bbfae3bae9628575cc1d2ba3d3ff6228148
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-09-13 14:00:38 +0200

    feat: create database seeder and add packages to table from config/packages.php

---
+++ database/seeders/PackagesTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Package;
+
+class PackagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $packages = config('packages');
+
+        foreach ($packages as $package) {
+            $new_package = new Package();
+            $new_package->destination = $package['destination'];
+            $new_package->description = $package['description'];
+            $new_package->price = $package['price'];
+            $new_package->days = $package['days'];
+            $new_package->accommodation = $package['accommodation'];
+            $new_package->transport = $package['transport'];
+            $new_package->departure_location = $package['departure_location'];
+            $new_package->departure_date = $package['departure_date'];
+            $new_package->return_date = $package['return_date'];
+            $new_package->max_participants = $package['max_participants'];
+
+            dump($new_package);
+
+        }
+
+    }
+}
